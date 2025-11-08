124f0d4638415326567c5a4112732871a2b13e17
Kaito <huan27042003@gmail.com>
2025-11-08 16:42:12 +0700

    first commit

---
+++ src/main/java/saintgiong/jobapplicant/common/dto/demo_producer/MessageRequestDto.java
@@ -0,0 +1,13 @@
+package saintgiong.jobapplicant.common.dto.demo_producer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageRequestDto {
+    private String message;
+}
+
