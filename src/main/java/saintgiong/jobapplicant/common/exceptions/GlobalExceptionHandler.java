8d75c08109f04e682ccc30edb6b5de2e1fe0d089
Mudoker <huuquoc7603@gmail.com>
2025-12-18 22:04:01 +0700

    feat: dynamically map error code to http status

--- src/main/java/saintgiong/jobapplicant/common/exceptions/GlobalExceptionHandler.java
+++ src/main/java/saintgiong/jobapplicant/common/exceptions/GlobalExceptionHandler.java
@@ -27,7 +27,12 @@ public class GlobalExceptionHandler {
                 .message(ex.getMessage())
                 .timestamp(LocalDateTime.now())
                 .build();
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+
+        HttpStatus httpStatus = HttpStatus.resolve(ex.getCode());
+        if (httpStatus == null) {
+            httpStatus = HttpStatus.BAD_REQUEST;
+        }
+        return new ResponseEntity<>(response, httpStatus);
     }
 
     @ExceptionHandler(Exception.class)
